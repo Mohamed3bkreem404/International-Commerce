6e2e3716d22658cdaee83f1cb033320a64c91781
Mohamed AbdElkareem <neutmoha@gmail.com>
2026-04-17 19:15:39 +0200

    K8S fully features 100% x)

--- k8s-old/dep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-for deploymentName in api auth cart frontend order payment product
-do
-   kubectl apply -f "$BASE_DIR/$deploymentName.yaml"
-done
