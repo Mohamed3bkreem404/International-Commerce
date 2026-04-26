ee59305236c3e19f754864d95bcb998166226872
Mohamed AbdElkareem <neutmoha@gmail.com>
2026-04-26 23:57:32 +0300

    idk

--- User Service/User-Service/src/main/java/com/example/User/Service/config/SecurityConfig.java
+++ User Service/User-Service/src/main/java/com/example/User/Service/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -46,7 +47,8 @@ public class SecurityConfig {
                             .anyRequest().authenticated();
 
                 })
-
+                .anonymous(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .authenticationManager(authenticationManager(httpSecurity));
 //                .httpBasic(Customizer.withDefaults());
