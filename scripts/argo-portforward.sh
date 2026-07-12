d04e71448778de11fe23700fecfa62881df33f22
Mohamed AbdElkareem <neutmoha@gmail.com>
2026-07-13 02:31:08 +0300

    argocd init

---
+++ scripts/argo-portforward.sh
@@ -0,0 +1,5 @@
+echo "Password : "
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+echo
+echo "running port-forward"
+kubectl port-forward service/argocd-server -n argocd 8080:443
