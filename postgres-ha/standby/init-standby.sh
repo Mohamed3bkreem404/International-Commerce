d3fe1e7db5ec3e51a8427fee100bb06238237e1f
Mohamed AbdElkareem <neutmoha@gmail.com>
2026-08-03 17:48:29 +0300

    modified standyby script

--- postgres-ha/standby/init-standby.sh
+++ postgres-ha/standby/init-standby.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
 
-rm -rf /var/lib/postgresql/data/*
+DATA_DIR="/var/lib/postgresql/data"
 
-PGPASSWORD=replica_pass pg_basebackup \
-  -h order_db \
-  -D /var/lib/postgresql/data \
-  -U replicator \
-  -Fp \
-  -Xs \
-  -P \
-  -R
+if [ ! -s "$DATA_DIR/PG_VERSION" ]; then
+    echo "Initializing standby from primary..."
+
+    rm -rf "${DATA_DIR:?}"/*
+
+    PGPASSWORD=replica_pass pg_basebackup \
+        -h order_db \
+        -p 5432 \
+        -U replicator \
+        -D "$DATA_DIR" \
+        -Fp \
+        -Xs \
+        -P \
+        -R
+fi
+
+echo "Starting PostgreSQL standby..."
+
+exec postgres
